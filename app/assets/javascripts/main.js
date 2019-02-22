e86706135ad183e94f4ed850400ccb6bc7b9084a
Connor O'Doherty <connor@jetinsight.com>
2019-02-21 22:09:02 -0800

    Ignore eit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,28 +1,26 @@
-var ready = function(scope) {
-    scope.find('.autocomplete-text-field').on("autocompleteresponse", function (event, ui) {
-        if (!$(event.target).is(':focus') && ui.content.length > 0) {
-            $(event.target).val(ui.content[0].value);
-            $($(event.target).attr('data-id-element')).val(ui.content[0].id);
-            if (ui.content[0].id) {
-                $($(event.target).attr('data-id-element')).trigger('change');
-            }
+$('.autocomplete-text-field').on("autocompleteresponse", function (event, ui) {
+    if (!$(event.target).is(':focus') && ui.content.length > 0) {
+        $(event.target).val(ui.content[0].value);
+        $($(event.target).attr('data-id-element')).val(ui.content[0].id);
+        if (ui.content[0].id) {
+            $($(event.target).attr('data-id-element')).trigger('change');
         }
-    });
+    }
+});
 
-    scope.find('.autocomplete-text-field').bind('railsAutocomplete.select', function (event, data) {
-        $($(this).attr('data-id-element')).val(data.item.id);
-        if (data.item.id) {
-            $($(this).attr('data-id-element')).trigger('change');
-        }
-    });
+$('.autocomplete-text-field').bind('railsAutocomplete.select', function (event, data) {
+    $($(this).attr('data-id-element')).val(data.item.id);
+    if (data.item.id) {
+        $($(this).attr('data-id-element')).trigger('change');
+    }
+});
 // attempt to deal with autofocus problem for rails 3 autocomplete gem
 // bug is trigged by a fresh reload of the page with an autocomplete with autofocus
 // https://github.com/crowdint/rails3-jquery-autocomplete/issues/185
-    scope.find('input[data-autocomplete][autofocus]').focus();
-};
+$('input[data-autocomplete][autofocus]').focus();
 
 
-$(document).on('turbolinks:load', ready);
+// $(document).on('turbolinks:load', ready);
 
 $(document).on('railsAutocomplete.select', '.autocomplete-navigate-to', function(event, data) {
     Turbolinks.visit(data.item.navigate_to);
